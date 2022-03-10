793e6298f58ef0d96ae467f7e3c076f2f8cf2250
NGUYEN VanTai <vantaifr@gmail.com>
2022-03-10 15:07:26 +0100

    BDD TRY/CATCH

--- Modele/bdd.php
+++ Modele/bdd.php
@@ -1,4 +1,7 @@
 <?php
+// define($servername,"localhost");
+// define($username,"root");
+// define($password,"");
     class BDD {
         private static ?BDD $instance = null;
         private static ?PDO $pdo = null;
@@ -13,19 +16,51 @@
             if(self::$instance == null) {
                 self::$instance = new BDD();
                 self::$pdo = new PDO('mysql:host=localhost', "root", "");
+                echo "Connected successfully<br>";
             }
             return self::$instance;
         }
         public function createBDD($nomBDD){
-            return self::$pdo->query("CREATE DATABASE $nomBDD");
+              try{
+                self::$pdo->exec("CREATE DATABASE $nomBDD");
+                echo "Database created successfully<br>";
+              } catch(PDOException $e) {
+                echo "Error" . $e->getMessage() . "<br>";
+              }
         }
         public function deleteBDD($nomBDD){
-            return self::$pdo->query("DROP DATABASE $nomBDD");
+            try{
+                self::$pdo->exec("DROP DATABASE $nomBDD");
+                echo "Database dropped successfully<br>";
+              } catch(PDOException $e) {
+                echo "Error" . $e->getMessage() . "<br>";
+              }
         }
 
         public function query($string){
             return self::$pdo->query($string);
         }
+
+        public function createTableTest()
+        {
+            $sql="CREATE Table User(
+                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                firstname VARCHAR(30) NOT NULL,
+                lastname VARCHAR(30) NOT NULL,
+            )";
+
+            if (self::$instance->query($sql)=== TRUE)
+            {
+                echo "created";
+            }
+            else
+            {
+                echo "error";
+            }
+        }
     }
     $db = BDD::getInstance();
+    //  $db->createBDD("mybd");
+    // $db->deleteBDD("mybd");
+
 ?>
