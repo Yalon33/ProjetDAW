3f185a5005c9cd6ca707a808d9ba0418845938a5
Daniel <danniel.pinson@gmail.com>
2022-03-14 14:08:30 +0100

    BDD suppression de fonction inutiles
    Le rôle de cette classe est de se connecter à une base existante, pas d'en créer une. C'est donc inutile de lui demander de créer une base de donnée et de créer des tables.

--- Modele/bdd.php
+++ Modele/bdd.php
@@ -6,61 +6,42 @@
         private static ?BDD $instance = null;
         private static ?PDO $pdo = null;
 
-        private function __construct() {}
+
+        private function __construct() {
+            // Le constructeur est privé pour éviter qu'une autre classe puisse appeller le constructeur
+            // Voir le patron de conception du singleton 
+            // https://fr.wikipedia.org/wiki/Singleton_(patron_de_conception)
+        }
+
 
         function __destruct() {
             self::$pdo = null;
         }
 
+
         public static function getInstance() {
             if(self::$instance == null) {
                 self::$instance = new BDD();
-                self::$pdo = new PDO('mysql:host=localhost', "root", "");
+                try{
+                    self::$pdo = new PDO('mysql:host=localhost', "root", "");
+                } catch (PDOException $e){
+                    printf("Error: " . $e->getMessage() . "<br>");
+                }
                 echo "Connected successfully<br>";
             }
             return self::$instance;
         }
-        public function createBDD($nomBDD){
-              try{
-                self::$pdo->exec("CREATE DATABASE $nomBDD");
-                echo "Database created successfully<br>";
-              } catch(PDOException $e) {
-                echo "Error" . $e->getMessage() . "<br>";
-              }
-        }
-        public function deleteBDD($nomBDD){
-            try{
-                self::$pdo->exec("DROP DATABASE $nomBDD");
-                echo "Database dropped successfully<br>";
-              } catch(PDOException $e) {
-                echo "Error" . $e->getMessage() . "<br>";
-              }
-        }
+
 
         public function query($string){
-            return self::$pdo->query($string);
-        }
-
-        public function createTableTest()
-        {
-            $sql="CREATE Table User(
-                id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                firstname VARCHAR(30) NOT NULL,
-                lastname VARCHAR(30) NOT NULL,
-            )";
-
-            if (self::$instance->query($sql)=== TRUE)
-            {
-                echo "created";
-            }
-            else
-            {
-                echo "error";
+            try{
+                return self::$pdo->query($string);
+            } catch (PDOException $e){
+                echo "Error : La requête SQL \"$string\" est eronée.<br>";
             }
         }
     }
     $db = BDD::getInstance();
-    //  $db->createBDD("mybd");
-    // $db->deleteBDD("mybd");
-
+    $test = $db->query("Wrong query");
+    //echo $test;
 ?>
