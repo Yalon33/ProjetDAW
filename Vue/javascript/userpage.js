c9ad27c6933271e94d7d30099de4fc12fdba5f5d
NGUYEN VanTai <vantaifr@gmail.com>
2022-03-14 15:12:11 +0100

    Corrige Error Menu

--- Vue/javascript/userpage.js
+++ Vue/javascript/userpage.js
@@ -1,10 +1,12 @@
 const icon_menu= document.querySelector(".icon_toggle"),
         nav= document.querySelector("nav"),
         body=document.querySelector("body"),
-        toggle_switch=document.querySelector(".toggle-switch");
-
-icon_menu.addEventListener("click",() =>
+        toggle_switch=document.querySelector(".toggle-switch"),
+        icon_hover = document.querySelector(".icon_hover"),
+        li_menu_user = document.querySelector("li.menu_user");
+icon_menu.addEventListener("click",(e) =>
 {
+    console.log(e.target);
     nav.classList.toggle("menu_sidebar");
 });
 toggle_switch.addEventListener("click",() =>
