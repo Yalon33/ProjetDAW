43cbb3ff6b5324914a376336bf0dc44f960c8810
dragnyon <dylanfaussot02@gmail.com>
2022-03-20 22:33:29 +0100

    test requete sql

--- Vue/html/validlogin.php
+++ Vue/html/validlogin.php
@@ -1,6 +1,25 @@
 <?php
-echo '<script type/javascript> alert("mdp incorrect donc ntm"); ';
-echo ' document.location.href="login.html";';
-echo '</script>';
 
+include("../../Modele/bdd.php");
+
+
+$id=3;
+$password="test";
+
+function veriftest($nom, $password)
+{
+    BDD::getinstance()->query("SELECT * FROM projet.eleve;");
+    echo "test effectuer";
+
+}
+
+
+veriftest($id, $password);
+
+
+
+
+//echo '<script type/javascript> alert("mdp incorrect donc ntm"); ';
+//echo ' document.location.href="login.html";';
+//echo '</script>';
 ?>
