f5bf7174203aec2071547c0e38ce37f82a2d56b9
Naillyk21 <Naillyk21@live.fr>
2022-03-21 10:42:22 +0100

    Recup login/mdp

--- Vue/html/validlogin.php
+++ Vue/html/validlogin.php
@@ -13,6 +13,18 @@ function veriftest($nom, $password)
 
 }
 
+//$_POST car on a choisit la method form dans login.html; 
+//Recupration du contenu de l'item form avec le "name" = login_form(défini dans login.html); 
+$contenuLogin =$_POST['login_form'] ; 
+//Recuperation du contenu de l'item form avec le "name" password_form (défini dans login.html); 
+$contenuMdp=$_POST['password_form']; 
+echo $contenuLogin; 
+echo $contenuMdp; 
+//Faire une requête avec le contenu du login/password 
+ 
+//Faire l'attribution des cookies en fonction de la connection / Et par défaut ? 
+ 
+ 
 
 veriftest($id, $password);
 
