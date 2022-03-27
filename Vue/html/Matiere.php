b6ecd3c252ac1eb3415b5353802ea2602ffdd073
Naillyk21 <Naillyk21@live.fr>
2022-03-27 19:01:03 +0200

    Matiere.php

---
+++ Vue/html/Matiere.php
@@ -0,0 +1,89 @@
+<?php
+    
+    class Matiere 
+    {
+        private $id;
+        private $nom;
+        private $dateCreation;
+        private $contenu;
+        private $createur;
+        private $tags;
+        private $niveau;
+
+        private function __construct($id,$nom,$dateCreation,$contenu,$createur,$tags,$niveau)
+        {
+            $this->$id = $id;
+            $this->$nom = $nom;
+            $this->$dateCreation = $dateCreation;
+            $this->$contenu = $contenu;
+            $this->$createur = $createur;
+            $this->$tags = $tags;
+            $this->$niveau = $niveau;
+            
+        }
+
+        public function setId($id)
+        {
+            $this->$id = $id;
+        }
+        public function setNom($nom)
+        {
+            $this->$nom = $nom;
+        }
+        public function setDateCreation($dateCreation)
+        {
+            $this->$dateCreation = $dateCreation;
+        }
+        public function setContenu($contenu)
+        {
+            $this->$contenu = $contenu;
+        }
+        public function setCreateur($createur)
+        {
+            $this->$createur = $createur;
+        }
+        public function setTags($tags)
+        {
+            $this->$tags = $tags;
+        }
+        public function setNiveau($niveau)
+        {
+            $this->$niveau = $niveau;
+        }
+
+        public function getId()
+        {
+            return $this->$id;
+        }
+        public function getNom()
+        {
+            return $this->$nom;
+        }
+        public function getDateCreation()
+        {
+            return $this->$dateCreation;
+        }
+        public function getContenu()
+        {
+            return $this->$contenu;
+        }
+        public function getCreateur()
+        {
+            return $this->$createur;
+        }
+        public function getTags()
+        {
+            return $this->$tags;
+        }
+        public function getNiveau()
+        {
+            return $this->$niveau;
+        }
+        
+    public function getSimilar()
+    {
+        
+    }
+    }
+
+?>
