a014f2efa776b9f6bb7f8018d859c62ce20413a8
Naillyk21 <Naillyk21@live.fr>
2022-03-27 18:27:31 +0200

    reponse.php

---
+++ Vue/html/Reponse.php
@@ -0,0 +1,31 @@
+<?php
+class Reponse 
+{
+    private id;
+    private reponse;
+
+    private function __construct($id,$reponse)
+    {
+        $this->$id = $id;
+        $this->$reponse = $reponse;
+    }
+    public function getId()
+    {
+        return $this->$id;
+    }
+    public function setId()
+    {
+        $this->$id = $id;
+    }
+     public function getReponse()
+    {
+        return $this->$reponse;
+    }
+    public function setId()
+    {
+        $this->$reponse = $reponse;
+    }
+}
+
+
+?>
