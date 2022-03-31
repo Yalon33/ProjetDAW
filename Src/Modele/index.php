7fff228209bda0a499105312faf72388cbb94bb5
Daniel <danniel.pinson@gmail.com>
2022-03-31 20:49:17 +0200

    Réfactorisation
    Moins de copier coller c'est mieux

--- Src/Modele/index.php
+++ Src/Modele/index.php
@@ -4,49 +4,26 @@
     define("SERVER", new Creationbase());
     define("ATTRIBUTS", "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL");
 
+    function createTable($array, $nomTable, $conditionsFin){
+        $stringAttributs = "";
+        foreach($array as $key => $type){
+            $stringAttributs .= $key . " " . $type . ", ";
+        }
+        $stringAttributs .= $conditionsFin;
+        SERVER->createTable(NOMBDD, "utilisateur", $stringAttributs);
+    }
+
     function createProjetTable(){
         SERVER->createBDD("projet");
         echo "Creation des tables pour le projet<br>";
-        $arrayUtilisateur = ["login" => "VARCHAR(50)",
-                                "mdp" => "VARCHAR(50)",
-                                "mail" => "VARCHAR(50)",
-                                "prenom" => "VARCHAR(50)",
-                                "nom" => "VARCHAR(50)",
-                                "typeUtilisateur" => "ENUM('Etudiant', 'Professeur')"];
-        $stringAttributsUtilisateur = "";
-        foreach($arrayUtilisateur as $key => $type){
-            $stringAttributsUtilisateur .= $key . " " . $type . ", ";
-        }
-        $stringAttributsUtilisateur .= "PRIMARY KEY(login, mdp)";
-        $arrayEleve = ["id" => "INT(6)",
-                        "niveau" => "VARCHAR(50)",
-                        "nDossier" => "INT(20)",
-                        "dateNaissance" => "VARCHAR(59)",
-                        "universite" => "VARCHAR(50)",
-                        "villeEtablissement" => "VARCHAR(50)",
-                        "cycle" => "VARCHAR(5)",
-                        "anneeEtude" => "VARCHAR(60)",
-                        "baccalaureat" => "VARCHAR(50)",
-                        "aneeBAC" => "VARCHAR(50)",
-                        "metionBAC" => "VARCHAR(50)",
-                        "etablissementBAC" => "VARCHAR(50)",
-                        "numeroPortable" => "INT(12)",
-                        "matiereSuivies" => "VARCHAR(50)"];
-        $stringAttributsEleve = "";
-        foreach($arrayEleve as $key => $type){
-            $stringAttributsEleve .= $key . " " . $type . ", ";
-        }
-        $stringAttributsEleve .= "PRIMARY KEY(id)";
-        $hashTable = ["utilisateur" => $stringAttributsUtilisateur,
-                "eleve" => $stringAttributsEleve];
-        foreach ($hashTable as $key => $val){
-            SERVER->createTable(NOMBDD, $key, $val);
-            echo "Creation de la table $key<br>";
-        }
+        $arrayUtilisateur = ["login" => "VARCHAR(50)", "mdp" => "VARCHAR(50)", "mail" => "VARCHAR(50)", "prenom" => "VARCHAR(50)", "nom" => "VARCHAR(50)", "typeUtilisateur" => "ENUM('Etudiant', 'Professeur')"];
+        $arrayEleve = ["id" => "INT(6)", "niveau" => "VARCHAR(50)", "nDossier" => "INT(20)", "dateNaissance" => "VARCHAR(59)", "universite" => "VARCHAR(50)", "villeEtablissement" => "VARCHAR(50)", "cycle" => "VARCHAR(5)", "anneeEtude" => "VARCHAR(60)", "baccalaureat" => "VARCHAR(50)", "aneeBAC" => "VARCHAR(50)", "metionBAC" => "VARCHAR(50)", "etablissementBAC" => "VARCHAR(50)", "numeroPortable" => "INT(12)", "matiereSuivies" => "VARCHAR(50)"];
+        createTable($arrayUtilisateur, "utilisateur", "PRIMARY KEY(login, mdp)");
+        createTable($arrayEleve, "eleve", "PRIMARY KEY(id)");
+        /*
         $Daniel = array("'Zokey', 'mdp', 'mail@mail.com', 'Daniel', 'Pinson', 'Etudiant'",
                 "'Pseudo2', 'autreMDP', 'mail2@mail.com', 'Louis', 'CroixVBaton', 'Etudiant'");
         //SERVER->insertData(NOMBDD, );
-        /*
         SERVER->insertData(NOMBDD, "eleve",
         "niveau, nDossier, dateNaissance, universite, villeEtablissement, cycle, anneeEtude, baccalaureat, anneeBAC, mentionBAC, etablissementBAC, numeroPortable, matiereSuivies",
         array("'L2', '12345678', '05/10/2000', 'Universite de Bourgogne-Franche-Comte', 'Dijon', 'License informatique', 'L2', 'BAC', '2018', 'Bien', 'Eiffel', '0652435145', 'Math'",
