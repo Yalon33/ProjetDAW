37900a8357b3e9df4a44c80897b7057a317c96fd
Daniel <danniel.pinson@gmail.com>
2022-04-04 08:48:38 +0200

    Refactorisation, bug fix, tests
    création de constantes pour refactoriser
    bdd appelle getInstance() pour éviter les cas où l'instance est nulle

--- Src/Modele/bdd.php
+++ Src/Modele/bdd.php
@@ -28,7 +28,7 @@
                 } catch (PDOException $e){
                     exit("Error: Impossible de se connecter au serveur sql, vérifier qu'il a bien été lancé et voir le message ci-dessous:<br>" . $e->getMessage() . "<br>");
                 }
-                echo "Connexion avec le serveur établie<br>";
+                //echo "Connexion avec le serveur établie<br>";
             }
             return self::$instance;
         }
@@ -40,9 +40,9 @@
          */
         public static function query($string){
             try{
-                return self::$pdo->query($string);
+                return self::getInstance()::$pdo->query($string);
             } catch (PDOException $e){
-                echo "<br>Error : La requête SQL \"$string\" est eronée. Voir le message d'erreur suivant : <br><t><b>" . $e->getMessage() . "</b>";
+                echo "Error : La requête SQL \"$string\" est eronée. Voir le message d'erreur suivant : <br><t><b>" . $e->getMessage() . "</b><br>";
             }
         }
 
@@ -53,13 +53,13 @@
          */
         public static function prepAndExec($query, $array){
             try{
-                $q = self::$pdo->prepare($query);
+                $q = self::getInstance()::$pdo->prepare($query);
                 $q->execute($array);
                 return $q->fetchAll();
             } catch (PDOException $e){
-                echo "<br>Error : La requête SQL \"$query\" avec le tableau \"";
+                echo "Error : La requête SQL \"$query\" avec le tableau <pre>";
                 print_r($array);
-                echo "\" est eronée. Voir le message d'erreur suivant : <br><t><b>" . $e->getMessage() . "</b><br>";
+                echo "</pre>est eronée. Voir le message d'erreur suivant : <br><t><b>" . $e->getMessage() . "</b><br>";
             }
         }
     }
--- Src/Modele/index.php
+++ Src/Modele/index.php
@@ -1,16 +1,36 @@
+<body style='background-color:black;'>
+<style>
+    Body {
+        color:grey;
+    }
+</style>
 <?php
     include("./creationbase.php");
-    define("NOMBDD", "projet");
+    define("PROJET", "projet");
     define("SERVER", new Creationbase());
-    define("ATTRIBUTS", "id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, firstname VARCHAR(30) NOT NULL, lastname VARCHAR(30) NOT NULL");
+    define("TABLES", array(
+        ["nomTable" => "utilisateur", "conditionFin" => "PRIMARY KEY(login, mdp)", "attributs" => ["login" => "VARCHAR(64)", "mdp" => "VARCHAR(64)", "mail" => "VARCHAR(64)", "prenom" => "VARCHAR(64)", "nom" => "VARCHAR(64)", "typeUtilisateur" => "ENUM('Etudiant', 'Professeur')"]],
+        ["nomTable" => "eleve", "conditionFin" => "PRIMARY KEY(id)", "attributs" => ["id" => "INT(8)", "niveau" => "VARCHAR(64)", "nDossier" => "INT(20)", "dateNaissance" => "VARCHAR(59)", "universite" => "VARCHAR(64)", "villeEtablissement" => "VARCHAR(64)", "cycle" => "VARCHAR(5)", "anneeEtude" => "VARCHAR(60)", "baccalaureat" => "VARCHAR(64)", "aneeBAC" => "VARCHAR(64)", "metionBAC" => "VARCHAR(64)", "etablissementBAC" => "VARCHAR(64)", "numeroPortable" => "INT(12)", "matiereSuivies" => "VARCHAR(64)"]],
+        ["nomTable" => "forum", "conditionFin" => "PRIMARY KEY(canaux)", "attributs" => ["id" => "INT(8)", "nom" => "VARCHAR(64)", "participant" => "INT(20)"]],
+        ["nomTable" => "canal", "conditionFin" => "PRIMARY KEY(id)", "attributs" => ["canaux" => "VARCHAR(50)"]],
+        ["nomTable" => "matiere", "conditionFin" => "PRIMARY KEY(id)", "attributs" => ["id" => "INT(8)", "nom" => "VARCHAR(64)", "dateCreation" => "VARCHAR(64)", "contenu" => "VARCHAR(64)", "createur" => "VARCHAR(64)", "tags" => "VARCHAR(64)", "niveau" => "VARCHAR(64)"]],
+        ["nomTable" => "message", "conditionFin" => "PRIMARY KEY(id)", "attributs" => ["id" => "INT(8)", "contenu" => "VARCHAR(64)"]],
+        ["nomTable" => "reponse", "conditionFin" => "PRIMARY KEY(id)", "attributs" => ["id" => "INT(8)", "reponse" => "VARCHAR(1024)"]]
+    ));
 
-    function createTable($array, $nomTable, $conditionsFin){
+    function createTable($nomTable, $conditionsFin, $array){
+        GLOBAL $SUCCES;
+        GLOBAL $ECHEC;
         $stringAttributs = "";
         foreach($array as $key => $type){
             $stringAttributs .= $key . " " . $type . ", ";
         }
         $stringAttributs .= $conditionsFin;
-        SERVER->createTable(NOMBDD, $nomTable, $stringAttributs);
+        SERVER->createTable(PROJET, $nomTable, $stringAttributs);
+    }
+
+    function createData($nomTable, $values, $arrayData){
+
     }
 
     /**
@@ -19,21 +39,15 @@
      * @return void
      */
     function createProjetTable(){
-        SERVER->createBDD("projet");
-        createTable(["login" => "VARCHAR(64)", "mdp" => "VARCHAR(64)", "mail" => "VARCHAR(64)", "prenom" => "VARCHAR(64)", "nom" => "VARCHAR(64)", "typeUtilisateur" => "ENUM('Etudiant', 'Professeur')"], "utilisateur", "PRIMARY KEY(login, mdp)");
-        createTable(["id" => "INT(8)", "niveau" => "VARCHAR(64)", "nDossier" => "INT(20)", "dateNaissance" => "VARCHAR(59)", "universite" => "VARCHAR(64)", "villeEtablissement" => "VARCHAR(64)", "cycle" => "VARCHAR(5)", "anneeEtude" => "VARCHAR(60)", "baccalaureat" => "VARCHAR(64)", "aneeBAC" => "VARCHAR(64)", "metionBAC" => "VARCHAR(64)", "etablissementBAC" => "VARCHAR(64)", "numeroPortable" => "INT(12)", "matiereSuivies" => "VARCHAR(64)"], "eleve", "PRIMARY KEY(id)");
-        //createTable([], "professeur", "");
-        createTable(["id" => "INT(8)", "nom" => "VARCHAR(64)", "participant" => "INT(20)"], "canal", "PRIMARY KEY(id)");
-        createTable(["canaux" => "VARCHAR(50)"], "forum", "PRIMARY KEY(canaux)");
-        createTable(["id" => "INT(8)", "nom" => "VARCHAR(64)", "dateCreation" => "VARCHAR(64)", "contenu" => "VARCHAR(64)", "createur" => "VARCHAR(64)", "tags" => "VARCHAR(64)", "niveau" => "VARCHAR(64)"], "matiere", "PRIMARY KEY(id)");
-        createTable(["id" => "INT(8)", "contenu" => "VARCHAR(64)"], "message", "PRIMARY KEY(id)");
-        createTable(["id" => "INT(8)", "reponse" => "VARCHAR(1024)"], "reponse", "PRIMARY KEY(id)");
+        foreach(TABLES as $table){
+            createTable($table["nomTable"], $table["conditionFin"], $table["attributs"]);
+        }
         echo "Creation des tables pour le projet réussi!<br>";
         /*
         $Daniel = array("'Zokey', 'mdp', 'mail@mail.com', 'Daniel', 'Pinson', 'Etudiant'",
                 "'Pseudo2', 'autreMDP', 'mail2@mail.com', 'Louis', 'CroixVBaton', 'Etudiant'");
-        //SERVER->insertData(NOMBDD, );
-        SERVER->insertData(NOMBDD, "eleve",
+        //SERVER->insertData(PROJET, );
+        SERVER->insertData(PROJET, "eleve",
         "niveau, nDossier, dateNaissance, universite, villeEtablissement, cycle, anneeEtude, baccalaureat, anneeBAC, mentionBAC, etablissementBAC, numeroPortable, matiereSuivies",
         array("'L2', '12345678', '05/10/2000', 'Universite de Bourgogne-Franche-Comte', 'Dijon', 'License informatique', 'L2', 'BAC', '2018', 'Bien', 'Eiffel', '0652435145', 'Math'",
         "'L3', '12345679', '05/11/2000', 'Universite de Bourgogne-Franche-Comte', 'Dijon', 'License mathématique', 'L3', 'BAC', '2017', 'TBien', 'Carnot', '0652435144', 'Français'"));
@@ -41,13 +55,17 @@
         */
     }
 
-    //launchTestSuite();
-    // Vider toutes les tables des informations qui sont dedans
-    SERVER->deleteData(NOMBDD, "eleve");
-    SERVER->deleteData(NOMBDD, "utilisateur");
-    createProjetTable();
-    //SERVER->deleteNOMBDD("projet");
-    //deleteAllTestTable();
+    function createProjetData(){
+        //createData("utilisateur", "");
+    }
 
+    //echo BDD::query("SELECT COUNT(*) FROM projet.eleve;")->fetchAll()[0][0] == 0 ? true : false;
+    //createTable(["nom" => "VARCHAR(64)", "prenom" => "VARCHAR(64)"], "testEleve", "PRIMARY KEY(nom)");
+    //SERVER->insertData("projet", "testeleve", "nom, prenom", "'Pinson', 'Daniel'");
+    //$q = BDD::prepAndExec("SELECT COUNT(*) FROM projet.testeleve WHERE nom=:nom;", array("nom" => "Pinson"));
+    //print_r($q);
+    //SERVER->createBDD("projet");
+    createProjetTable();
+    createProjetData();
 ?>
 </body>
